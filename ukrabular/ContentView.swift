c28fce9ed4ce85efefaa60f912cd8bf3f3614383
Maximilian Bieleke <maxbieleke@web.de>
2025-11-06 00:39:16 +0100

    Initial Commit

---
+++ ukrabular/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ukrabular
+//
+//  Created by Maximilian Bieleke on 06.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ukrabular/ukrabularApp.swift
@@ -0,0 +1,17 @@
+//
+//  ukrabularApp.swift
+//  ukrabular
+//
+//  Created by Maximilian Bieleke on 06.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ukrabularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
